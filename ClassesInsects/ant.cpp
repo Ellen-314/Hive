36d5276a7d4c2a3e16dfdc7143518d0cccec5897
04may <maryne.comblat@etu.utc.fr>
2024-11-04 23:03:59 +0100

    Update ant.cpp

--- ClassesInsects/ant.cpp
+++ ClassesInsects/ant.cpp
@@ -6,11 +6,11 @@ unsigned int Ant::poseNoir = 0;
 
 std::vector<const BoardSpot*> Ant::moov(int x, int y, const Board& board){
 std::vector <const BoardSpot*> possibilite;
-std::queue <const BoardSpot*> Averifier;//queue permet de creer FIFO 
-std::set <const BoardSpot*> verifie; //set peremet le tri et l'unicité des éléments 
+std::queue <const BoardSpot*> Averifier;//queue permet de creer FIFO
+std::set <const BoardSpot*> verifie; //set peremet le tri et l'unicité des éléments
 const BoardSpot* Spotfourmi = board.getSpot(x, y);
 
-Averifier.push(Spotfourmi); //push l'ajoute à la fin 
+Averifier.push(Spotfourmi); //push l'ajoute à la fin
 verifie.insert(Spotfourmi);// ajoute uniquement si il n'est pas déjà dans la liste
 
  while (!Averifier.empty()) {
@@ -21,23 +21,22 @@ verifie.insert(Spotfourmi);// ajoute uniquement si il n'est pas déjà dans la l
         std::vector<const BoardSpot*> voisins = board.voisinsNull(
             currentSpot->getCoordinates().first,
             currentSpot->getCoordinates().second
-        ); 
+        );
 
         for (size_t i = 0; i < voisins.size(); i++) {
             // visite.find(voisins[i]) renvoie visite.end() uniquement si le voisin n'a pas déjà été visité
-            if (visite.find(voisins[i]) == visite.end()) {
+            if (verifie.find(voisins[i]) == verifie.end()) {
                 int surroundingInsects = board.trouverVoisinsInsects(
                     voisins[i]->getCoordinates().first,
                     voisins[i]->getCoordinates().second
                 ).size(); //surrondingInsects indique combien d'insects il y a autour de l'emplacement
                         if (surroundingInsects < 5) {// si il y a plus de 5 insects autour de l'emplacement la fourmi ne peut pas y acceder
-                            possibilite.push_back(neighbor);
-                            Avisiter.push(neighbor);
-                            visite.insert(neighbor);
+                            possibilite.push_back(voisins[i]);
+                            Averifier.push(voisins[i]);
+                            verifie.insert(voisins[i]);
                         }
                     }
         }
     }
     return possibilite;
 }
-
