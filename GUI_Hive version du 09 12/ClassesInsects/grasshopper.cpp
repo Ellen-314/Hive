e2770458d024237ddcfff3ee561a76a793273f08
Ellen-314 <150928225+Ellen-314@users.noreply.github.com>
2024-12-10 00:03:06 +0100

    Update grasshopper

---
+++ GUI_Hive version du 09 12/ClassesInsects/grasshopper.cpp
@@ -0,0 +1,53 @@
+#include "grasshopper.h"
+#include "../Board.h"
+
+const unsigned int Grasshopper::Max =3;
+unsigned int Grasshopper::poseBlanc = 0;
+unsigned int Grasshopper::poseNoir = 0;
+
+
+std::vector<const BoardSpot*> Grasshopper::moov(int x, int y, const Board& board)const
+{
+    std::vector<const BoardSpot*> possibleMoves;
+    const BoardSpot* currentSpot = board.getSpot(x, y);
+
+    std::pair<int, int> directions[] = {
+        {1, 0},   // (x + 1, y)
+        {1, 1},   // (x + 1, y + 1)
+        {0, 1},   // (x, y + 1)
+        {-1, 0},  // (x - 1, y)
+        {-1, -1}, // (x - 1, y - 1)
+        {0, -1}   // (x, y - 1)
+    };
+
+    // Parcours des directions
+    for (const auto& dir : directions) {
+        int newX = x + dir.first;
+        int newY = y + dir.second;
+        const BoardSpot* nextSpot = currentSpot;
+
+
+        // Avancer dans la direction tant que la case suivante contient un insecte
+        while (nextSpot) {
+            nextSpot = board.getSpot(nextSpot->getCoordinates().first + newX,
+                                     nextSpot->getCoordinates().second + newY);
+
+            // Arr�te si la case est vide ou invalide (en dehors des limites)
+            if (!nextSpot || !nextSpot->hasInsect()) {
+                break;
+            }
+        }
+
+        // Si une case vide est trouv�e, elle est ajout�e aux mouvements possibles
+        if (nextSpot && !nextSpot->hasInsect()) {
+            possibleMoves.push_back(nextSpot);
+        }
+    }
+    return possibleMoves;
+
+}
+
+bool Grasshopper::estPasAuMax(bool couleur){
+    if (couleur==0){ return getPoseNoir()< getMax(); }
+    else{ return getPoseBlanc()< getMax(); }
+}
