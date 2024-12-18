2cfab6e05343c72f868e5539b7924074a71024be
Ellen-314 <150928225+Ellen-314@users.noreply.github.com>
2024-12-18 16:32:33 +0100

    Ajout connexité Qt

--- GUI_Hive version du 17 12/Hex.cpp
+++ GUI_Hive version du 17 12/Hex.cpp
@@ -66,63 +66,72 @@ void Hex::unSetColor() {
     setBrush(Qt::NoBrush);
 }
 
-//gestion de posage et des déplacements graphiques des insects
 void Hex::mousePressEvent(QGraphicsSceneMouseEvent *event){
     Board& board = game->getPlateau();
-    if(getIsPlaced()&& getOwner() == game->getWhosTurn()){
-        if(!getIsEmpty()&& getOwner() == game->getWhosTurn()){
+    if(getIsPlaced() && getOwner() == game->getWhosTurn()){
+        if(!getIsEmpty() && getOwner() == game->getWhosTurn()){
             HexBoard* hexboard = game->hexBoard;
             hexboard->eraseHighlighted();
-            qDebug()<<getCoord();
-        //mettre possibilités de move
-        const BoardSpot* spot = nullptr;
-        spot = board.getSpot(getCoord().x(), getCoord().y());
-        qDebug()<<spot->getCoordinates();
-        qDebug()<<spot->getInsect();
+            qDebug() << getCoord();
+            // mettre possibilités de move
+            const BoardSpot* spot = nullptr;
+            spot = board.getSpot(getCoord().x(), getCoord().y());
+            qDebug() << spot->getCoordinates();
+            qDebug() << spot->getInsect();
 
-        std::vector <const BoardSpot*> possibilite = spot->getInsect()->moov(getCoord().x(), getCoord().y(), board);
-        for(const auto& spot : possibilite){
-            auto [x,y] = spot->getCoordinates();
-            Hex& hex = hexboard->getHex(QPoint(x, y));
-            qDebug()<<"x:"<<x<<" y:"<<y;
-            hexboard->addHighlightedHex(&hex, "magenta");
+            std::vector<const BoardSpot*> possibilite = spot->getInsect()->moov(getCoord().x(), getCoord().y(), board);
+            for(const auto& spot : possibilite){
+                auto [x, y] = spot->getCoordinates();
+
+                // Création d'un board temporaire pour simuler le move
+                Board tempBoard = board;
+                tempBoard.moovInsect(getCoord().x(), getCoord().y(), x, y);
+
+                // Verifie que le board est tj connexe si on fait le move
+                if (tempBoard.isConnexe()) {
+                    Hex& hex = hexboard->getHex(QPoint(x, y));
+                    qDebug() << "x:" << x << " y:" << y;
+                    hexboard->addHighlightedHex(&hex, "magenta");
+                }
+            }
+            game->pawnToMove = this;
+            return;
         }
-        game->pawnToMove = this;
-
-        return;
-
-
-    }
     }
 
-    if(this->isEmpty && game->pawnToMove!=NULL && game->hexBoard->inHighlighted(this)){
-        game->getPlateau().print_debug();
-        game->getPlateau().moovInsect(game->pawnToMove->getCoord().x(), game->pawnToMove->getCoord().y(), this->getCoord().x(), this->getCoord().y());
-        game->getPlateau().print_debug();
-        game->getPlateau().addNullSpot(this->getCoord().x(),this->getCoord().y());
-        game->exchangeSpot(game->hexBoard, game->pawnToMove, this);
-        game->pawnToMove=NULL;
-        game->hexBoard->eraseHighlighted();
-        game->nextPlayersTurn();
+    if(this->isEmpty && game->pawnToMove != NULL && game->hexBoard->inHighlighted(this)){
+        // Check si ça reste connexe
+        Board tempBoard = board; // Crée un board temporaire pour simuler le move
+        tempBoard.moovInsect(game->pawnToMove->getCoord().x(), game->pawnToMove->getCoord().y(), this->getCoord().x(), this->getCoord().y());
+        if (tempBoard.isConnexe()) {
+            game->getPlateau().print_debug();
+            game->getPlateau().moovInsect(game->pawnToMove->getCoord().x(), game->pawnToMove->getCoord().y(), this->getCoord().x(), this->getCoord().y());
+            game->getPlateau().print_debug();
+            game->getPlateau().addNullSpot(this->getCoord().x(), this->getCoord().y());
+            game->exchangeSpot(game->hexBoard, game->pawnToMove, this);
+            game->pawnToMove = NULL;
+            game->hexBoard->eraseHighlighted();
+            game->nextPlayersTurn();
+        } else {
+            qDebug() << "Move pas autorisé: Le board ne sera pas connexe.";
+        }
         return;
-}
-
+    }
 
     // si hex est pas placé (=pion) alors le prendre
-    if (getIsPlaced() == false && getOwner() == game->getWhosTurn() ){
-        game->pawnToMove=NULL;
+    if (getIsPlaced() == false && getOwner() == game->getWhosTurn()){
+        game->pawnToMove = NULL;
         HexBoard* hexboard = game->hexBoard;
         hexboard->eraseHighlighted();
         std::vector<const BoardSpot*> possibilites = board.possibleplacer(getCouleur());
         if(game->getColorToPlay() == 1){
-            if (board.getSpot(0, 0)==nullptr){
+            if (board.getSpot(0, 0) == nullptr){
                 board.addSpot(0, 0);
                 //std::cout << "\n=========\nDEBUG : Ajout du spot pour la couleur blanche à la position (0, 0)\n=========\n\n";
             }
             const BoardSpot* spot = board.getSpot(0, 0);
             possibilites.push_back(spot);
-        }
-        else{
+        } else {
             if (board.getSpot(0, 1) == nullptr) {
                 board.addSpot(0, 1);
                 //std::cout << "\n=========\nDEBUG : Ajout du spot pour la couleur noire à la position (0, 1)\n=========\n\n";
@@ -131,17 +140,14 @@ void Hex::mousePressEvent(QGraphicsSceneMouseEvent *event){
             possibilites.push_back(spot);
         }
         int x, y;
-        qDebug()<<possibilites.size();
+        qDebug() << possibilites.size();
         for(const auto& spot : possibilites){
-            auto [x,y] = spot->getCoordinates();
+            auto [x, y] = spot->getCoordinates();
             Hex& hex = hexboard->getHex(QPoint(x, y));
             hexboard->addHighlightedHex(&hex, "cyan");
-
-
         }
         game->pickUppawn(this);
     }
-
     // si il est pris alors le replacer
     else {
         if(game->hexBoard->inHighlighted(this)){
@@ -149,6 +155,8 @@ void Hex::mousePressEvent(QGraphicsSceneMouseEvent *event){
         }
     }
 }
+
+
 
 void Hex::setOwner(QString player){
     // MAJ owner
